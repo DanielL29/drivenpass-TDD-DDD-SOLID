18dbc60ab613947d6c35bfaf229b0af1ff3835fa
DanielL29 <danielnatsufire@gmail.com>
2022-11-22 23:10:52 -0300

    feat: user repository in memory and interfaces

---
+++ src/modules/users/repositories/interfaces/user-repo.ts
@@ -0,0 +1,14 @@
+import { RepositoryToAuth } from "@core/infra/repository";
+import { User } from "@modules/users/domain/user";
+
+export interface UserPersistence {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+export interface UserRepo extends RepositoryToAuth {
+  findByEmail(email: string): Promise<User | null>;
+}
