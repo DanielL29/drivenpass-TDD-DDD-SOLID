b650fa08cb7799eeea4fbe5c841c368805784711
DanielL29 <danielnatsufire@gmail.com>
2022-11-24 22:24:04 -0300

    feat: auth user and it route path

--- src/modules/users/infra/http/routes/user-route.ts
+++ src/modules/users/infra/http/routes/user-route.ts
@@ -1,4 +1,5 @@
 import { ExpressRoute } from "@core/infra/route";
+import { authUserController } from "@modules/users/use-cases/auth-user";
 import { createUserController } from "@modules/users/use-cases/create-user";
 import { SchemaValidator } from "@shared/infra/http/middlewares/request-schema-handler";
 
@@ -8,6 +9,11 @@ export class UserRoute extends ExpressRoute {
       "/sign-up",
       SchemaValidator.validateSchema("createUserSchema"),
       createUserController.execute
+    );
+    this.router.post(
+      "/sign-in",
+      SchemaValidator.validateSchema("authUserSchema"),
+      authUserController.execute
     );
   }
 }
