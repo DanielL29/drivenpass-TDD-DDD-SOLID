5ea756fa5c6ede8237ada3de9ad0d9c401e48a63
DanielL29 <danielnatsufire@gmail.com>
2022-11-25 00:15:01 -0300

    fix: change validation error type

--- src/shared/infra/http/middlewares/request-schema-handler.ts
+++ src/shared/infra/http/middlewares/request-schema-handler.ts
@@ -14,7 +14,7 @@ export class SchemaValidator {
       if (error) {
         const message = error.details.map((detail) => detail.message).join();
 
-        throw new CustomError("error_bad_request", message);
+        throw new CustomError("error_unprocessable_entity", message);
       }
 
       return next();
