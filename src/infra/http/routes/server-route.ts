982287f79642723f422b84a4488b44cbe14950a3
DanielL29 <danielnatsufire@gmail.com>
2022-11-30 18:19:55 -0300

    feat: add credentials route

--- src/infra/http/routes/server-route.ts
+++ src/infra/http/routes/server-route.ts
@@ -1,8 +1,10 @@
 import { ExpressRoute } from "@core/infra/route";
-import { UserRoute } from "@modules/users/infra/http/routes/user-route";
+import { CredentialRoute } from "@modules/credentials/infra/http/routes/credential-route";
+import { UserRoute } from "@shared/modules/users/infra/http/routes/user-route";
 
 export class ServerRoute extends ExpressRoute {
   public useRouter(): void {
     this.router.use("/users", new UserRoute().router);
+    this.router.use("/credentials", new CredentialRoute().router);
   }
 }
