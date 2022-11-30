1dd412d8fecd412837c44a7cad6ddebbd0daf300
DanielL29 <danielnatsufire@gmail.com>
2022-11-30 18:16:17 -0300

    feat: implementing new methods in core part

--- src/core/domain/use-case.ts
+++ src/core/domain/use-case.ts
@@ -1,17 +1,18 @@
-import { CreateUserDTO } from "@shared/dtos/users/create-user.dto";
-import { UserDTO } from "@shared/dtos/users/user.dto";
+// export interface UseCase<Request, Response> {
+//   create(req: Request, userId: string): Promise<Response>;
+//   findAll(userId: string): Promise<Response[]>;
+//   find(id: string, userId: string): Promise<Response>;
+//   remove(id: string, userId: string): Promise<Response>;
+// }
+
+// export interface UseCaseUser<Request, Response> {
+//   create(req: Request): Promise<Response>;
+// }
+
+// export interface UseCaseAuth<Request, Response> {
+//   login(req: Request): Promise<Response>;
+// }
 
 export interface UseCase<Request, Response> {
-  create(req: Request): Promise<Response>;
-  findAll(id: string): Promise<Response[]>;
-  find(id: string): Promise<Response>;
-  remove(id: string): Promise<Response>;
-}
-
-export interface UseCaseUser<Request, Response> {
-  create(req: Request): Promise<Response>;
-}
-
-export interface UseCaseAuth<Request, Response> {
-  login(req: Request): Promise<Response>;
+  execute(useCaseReq?: Request, userId?: string): Promise<Response>;
 }
