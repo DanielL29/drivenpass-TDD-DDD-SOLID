3b63d690d68f48f936cbfe9326d7c9887612ae5c
DanielL29 <danielnatsufire@gmail.com>
2022-12-01 16:02:45 -0300

    feat: find credential

---
+++ src/modules/credentials/use-cases/find-credential/find-credential.use-case.test.ts
@@ -0,0 +1,38 @@
+import { CustomError } from "@core/logic/error";
+import { faker } from "@faker-js/faker";
+import { Credential } from "@modules/credentials/domain/credential";
+import { InMemoryCredentialRepo } from "@modules/credentials/repositories/implements/in-memory-credential-repo";
+import { CredentialRepo } from "@modules/credentials/repositories/interfaces/credential-repo";
+import { createFakeCredentialDTO } from "../create-fake-credential-factory";
+import { FindCredentialUseCase } from "./find-credential.use-case";
+
+describe("find credential use case", () => {
+  let repo: CredentialRepo;
+  let sut: FindCredentialUseCase;
+
+  beforeEach(() => {
+    repo = new InMemoryCredentialRepo();
+    sut = new FindCredentialUseCase(repo);
+  });
+
+  it("should find a existing user credential", async () => {
+    const createCredentialReq = createFakeCredentialDTO();
+    const userId = faker.datatype.uuid();
+    const credential = Credential.create(createCredentialReq, userId);
+    const createdCredential = await repo.create(credential);
+
+    const result = await sut.execute(createdCredential._id, userId);
+
+    expect(result).toHaveProperty("id");
+    expect(result.id).toEqual(createdCredential._id);
+  });
+
+  it("should throw an error if not found user credential", async () => {
+    const credentialId = faker.datatype.uuid();
+    const userId = faker.datatype.uuid();
+
+    await expect(sut.execute(credentialId, userId)).rejects.toEqual(
+      new CustomError("error_not_found", "user credential not found")
+    );
+  });
+});
---
+++ src/modules/credentials/use-cases/find-credential/find-credential.use-case.ts
@@ -0,0 +1,38 @@
+import { UseCase } from "@core/domain/use-case";
+import { CustomError } from "@core/logic/error";
+import { Credential } from "@modules/credentials/domain/credential";
+import { CredentialMapper } from "@modules/credentials/mappers/credential-mapper";
+import { CredentialRepo } from "@modules/credentials/repositories/interfaces/credential-repo";
+import { CredentialDTO } from "@shared/dtos/credentials/credential.dto";
+
+export class FindCredentialUseCase implements UseCase<string, CredentialDTO> {
+  private readonly repo: CredentialRepo;
+  private readonly mapper: CredentialMapper;
+
+  constructor(repo: CredentialRepo) {
+    this.repo = repo;
+    this.mapper = new CredentialMapper();
+  }
+
+  private async findCredentialOrFail(
+    credentialId: string,
+    userId: string
+  ): Promise<Credential> {
+    const isCredential = await this.repo.find(credentialId, userId);
+
+    if (!isCredential) {
+      throw new CustomError("error_not_found", "user credential not found");
+    }
+
+    return isCredential;
+  }
+
+  public async execute(
+    credentialId: string,
+    userId: string
+  ): Promise<CredentialDTO> {
+    const credential = await this.findCredentialOrFail(credentialId, userId);
+
+    return this.mapper.toDTO(credential);
+  }
+}
