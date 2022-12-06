8f077feb84ea02ba974910d922c93beb6f0d32cc
DanielL29 <danielnatsufire@gmail.com>
2022-12-06 16:02:22 -0300

    feat: add note interfaces and in memory repo

---
+++ src/modules/notes/repositories/implements/in-memory-note-repo.ts
@@ -0,0 +1,48 @@
+import { Note } from "@modules/notes/domain/note";
+import { NoteMapper } from "@modules/notes/mappers/note-mapper";
+import { NotePersistence, NoteRepo } from "../interfaces/note-repo";
+
+export class InMemoryNoteRepo implements NoteRepo {
+  private notes: NotePersistence[];
+  private readonly mapper: NoteMapper;
+
+  constructor() {
+    this.notes = [];
+    this.mapper = new NoteMapper();
+  }
+
+  public async findByTitle(
+    title: string,
+    userId: string
+  ): Promise<Note | null> {
+    const isMemoryNote = this.notes.find(
+      (note) => note.title === title && note.userId === userId
+    );
+
+    if (!isMemoryNote) {
+      return null;
+    }
+
+    return this.mapper.toDomain(isMemoryNote);
+  }
+
+  public async create(data: Note): Promise<Note> {
+    const persistenceNote = this.mapper.toPersistence(data);
+
+    this.notes.push(persistenceNote);
+
+    return this.mapper.toDomain(persistenceNote);
+  }
+
+  public async findAll(userId: string): Promise<Note[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  public async find(id: string): Promise<Note | null> {
+    throw new Error("Method not implemented.");
+  }
+
+  public async remove(id: string): Promise<Note | null> {
+    throw new Error("Method not implemented.");
+  }
+}
