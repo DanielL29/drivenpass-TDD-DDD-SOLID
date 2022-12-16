bb6d9c5c8fa3939691e9c483975e0fc2ac863d80
DanielL29 <danielnatsufire@gmail.com>
2022-12-16 02:49:56 -0300

    feat: add card router to main router

--- src/infra/http/routes/server-route.ts
+++ src/infra/http/routes/server-route.ts
@@ -1,4 +1,5 @@
 import { ExpressRoute } from "@core/infra/route";
+import { CardRoute } from "@modules/cards/infra/http/routes/card-route";
 import { CredentialRoute } from "@modules/credentials/infra/http/routes/credential-route";
 import { NoteRoute } from "@modules/notes/infra/http/routes/note-route";
 import { UserRoute } from "@shared/modules/users/infra/http/routes/user-route";
@@ -8,5 +9,6 @@ export class ServerRoute extends ExpressRoute {
     this.router.use("/users", new UserRoute().router);
     this.router.use("/credentials", new CredentialRoute().router);
     this.router.use("/notes", new NoteRoute().router);
+    this.router.use("/cards", new CardRoute().router);
   }
 }
